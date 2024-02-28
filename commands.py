eb359b40ceff4a95b5c63db46d97a5731e79d8fe
goosetheory <sjgorders@gmail.com>
2024-02-28 11:26:34 +0100

    Remaining seed info. Fix error in empty-db command

--- commands.py
+++ commands.py
@@ -18,6 +18,56 @@ def seed_db():
         
         session.add_all([vegetarian_diet, vegan_diet, gluten_free_diet, paleo_diet])
 
+        print('Seeding eaters...')
+        michael = Eater(name='Michael', restrictions=[vegetarian_diet])
+        george_michael = Eater(name='George Michael', restrictions=[vegetarian_diet, gluten_free_diet])
+        lucile = Eater(name='Lucile', restrictions=[gluten_free_diet])
+        gob = Eater(name='Gob', restrictions=[paleo_diet])
+        tobias = Eater(name='Tobias')
+        maeby = Eater(name='Maeby', restrictions=[vegan_diet])
+
+        session.add_all([michael, george_michael, lucile, gob, tobias, maeby])
+
+        print('Seeding restaurants...')
+        lardo = Restaurant(name='Lardo', endorsements=[gluten_free_diet])
+        panaderia_rosetta = Restaurant(name='Panadería Rosetta', endorsements=[vegetarian_diet, gluten_free_diet])
+        tetetlan = Restaurant(name='Tetetlán', endorsements=[paleo_diet, gluten_free_diet])
+        falling_piano = Restaurant(name='Falling Piano Brewing Co')
+        # Note: anywhere that has a vegan diet is also vegetarian-friendly
+        utopia = Restaurant(name='u.to.pi.a', endorsements=[vegan_diet, vegetarian_diet])
+
+        session.add_all([lardo, panaderia_rosetta, tetetlan, falling_piano, utopia])
+
+        print('Seeding tables...')
+        tables = []
+        tables.extend(_create_tables(lardo, capacity=2, number=4))
+        tables.extend(_create_tables(lardo, capacity=4, number=2))
+        tables.extend(_create_tables(lardo, capacity=6, number=1))
+
+        tables.extend(_create_tables(panaderia_rosetta, capacity=2, number=3))
+        tables.extend(_create_tables(panaderia_rosetta, capacity=4, number=2))
+        
+        tables.extend(_create_tables(tetetlan, capacity=2, number=4))
+        tables.extend(_create_tables(tetetlan, capacity=4, number=2))
+        tables.extend(_create_tables(tetetlan, capacity=6, number=1))
+        
+        tables.extend(_create_tables(falling_piano, capacity=2, number=5))
+        tables.extend(_create_tables(falling_piano, capacity=4, number=5))
+        tables.extend(_create_tables(falling_piano, capacity=6, number=5))
+        
+        tables.extend(_create_tables(utopia, capacity=2, number=2))
+
+        session.add_all(tables)
+
+        print('Database seeding complete.')
+
+
+def _create_tables(restaurant, capacity, number):
+    tables = [Table(restaurant=restaurant, capacity=capacity) for _ in range(number)]
+    return tables
+
+
+
 @commands.cli.command('empty-db')
 @with_appcontext
 def empty_db():
@@ -33,11 +83,12 @@ def empty_db():
             return
 
         print('Emptying database...')
+        session.query(reservation_eaters).delete()
+        session.query(restrictions).delete()
+        session.query(endorsements).delete()
         session.query(Eater).delete()
         session.query(Restaurant).delete()
         session.query(Table).delete()
         session.query(Diet).delete()
         session.query(Reservation).delete()
-        session.query(reservation_eaters).delete()
-        session.query(restrictions).delete()
-        session.query(endorsements).delete()
+        print('Database emptied.')
--- db/table.py
+++ db/table.py
@@ -2,16 +2,21 @@ import uuid
 
 from .db import db
 
-from sqlalchemy import ForeignKey, Text
+from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
 
+# Note to Rec: Tables could also have been represented in a more "flat" way.
+# This table would instead be called "capacities", perhaps, and would
+# store a restaurant id, a capacity, and a number of tables at that restaurant
+# with that capacity. But this allows for tables to have attributes
+# other than capacity in the future, e.g. window seating, booth vs chairs, etc.
 class Table(db.Model):
     __tablename__ = 'tables'
 
     id: Mapped[uuid.uuid4] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     restaurant_id: Mapped[uuid.uuid4] = mapped_column(ForeignKey('restaurants.id'), nullable=False)
-    capacity: Mapped[int] = mapped_column(Text, nullable=False)
+    capacity: Mapped[int] = mapped_column(Integer, nullable=False)
 
-    restaurant = relationship('Restaurant', back_populates='tables')
+    restaurant = relationship('Restaurant', back_populates='tables', uselist=False)
     reservations = relationship('Reservation', back_populates='table')
